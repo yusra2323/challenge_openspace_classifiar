4b175b97f9f1603a5a30221fbcbf01f4e8db1a3e
Yusra Ali <yusra_ma@yahoo.com>
2024-09-27 15:17:24 +0200

    project

--- main.py
+++ main.py
@@ -3,20 +3,20 @@
 import csv
 import random as rd
 from utils.table import Seat ,Table
-from utils.opernspace import Openspace 
+from utils.openspace import Openspace 
 
 with open('new_colleagues.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    
     data=[]
     for row in spamreader:
        data.append(row)
+       data = [student for sublist in data for student in sublist]
+    data.remove('Duc')
+    print(len(data))
+     
     print(data)
 
-list_seats=[]
-for name in data:
-Seat = Seat()
-Seat.set_occupant(name)
-list_seats.append(seat)
 
 
 
